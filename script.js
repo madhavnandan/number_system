acaaab2a0521de7f173eafef5f4708bf8851aecd
Madhav Nandan Singh <99002875+madhavnandan@users.noreply.github.com>
2023-10-04 20:05:37 +0530

    Add files via upload
    Number System

---
+++ script.js
@@ -0,0 +1,347 @@
+/* 
+Author : Madhav Nandan Singh
+Date : 04/10/2023
+*/
+
+function display() {
+    var _0 = document.getElementById("_0");
+    var _1 = document.getElementById("_1");
+    var _2 = document.getElementById("_2");
+    var _3 = document.getElementById("_3");
+    var _4 = document.getElementById("_4");
+    var _5 = document.getElementById("_5");
+    var _6 = document.getElementById("_6");
+    var _7 = document.getElementById("_7");
+    var _8 = document.getElementById("_8");
+    var _9 = document.getElementById("_9");
+    var _10 = document.getElementById("_10");
+    var _11 = document.getElementById("_11");
+    var _12 = document.getElementById("_12");
+    var select = document.getElementById("myselect").value;
+    console.log(select);
+    if (select == 0) {
+        _0.style.display = "flex";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 1){
+        _0.style.display = "none";
+        _1.style.display = "flex";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 2){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "flex";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 3){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "flex";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 4){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "flex";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 5){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "flex";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 6){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "flex";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 7){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "flex";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 8){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "flex";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 9){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "flex";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 10){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "flex";
+        _11.style.display = "none";
+        _12.style.display = "none";
+    }
+    else if(select == 11){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "flex";
+        _12.style.display = "none";
+    }
+    else if(select == 12){
+        _0.style.display = "none";
+        _1.style.display = "none";
+        _2.style.display = "none";
+        _3.style.display = "none";
+        _4.style.display = "none";
+        _5.style.display = "none";
+        _6.style.display = "none";
+        _7.style.display = "none";
+        _8.style.display = "none";
+        _9.style.display = "none";
+        _10.style.display = "none";
+        _11.style.display = "none";
+        _12.style.display = "flex";
+    }
+    
+}
+
+function binaryOctal(){
+    var bnumber = document.getElementById("bnumber").value;
+        console.log(bnumber)
+    try {
+        var output = parseInt(bnumber , 2).toString(8);
+        console.log(output)
+        document.getElementById("Output").innerHTML = `Result is : ${output}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Binary Number")
+    }    
+}
+
+function binaryDecimal(){
+    var bnumber = document.getElementById("bnumber1").value;
+        console.log(bnumber)
+    try {
+        var output = parseInt(bnumber , 2);
+        console.log(output)
+        document.getElementById("Output1").innerHTML = `Result is : ${output}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Binary Number")
+    }    
+}
+
+function binaryHexa(){
+    var bnumber = document.getElementById("bnumber2").value;
+        console.log(bnumber)
+    try {
+        var output = parseInt(bnumber , 2).toString(16);
+        console.log(output)
+        document.getElementById("Output2").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Binary Number")
+    }    
+}
+
+function decimalBinary(){
+    var decimal = document.getElementById("decimal").value;
+    try {
+        var output = parseInt(decimal).toString(2);
+        console.log(output)
+        document.getElementById("output4").innerHTML = `Result is : ${output}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Decimal Number")
+    }    
+}
+
+function decimalOctal(){
+    var decimal = document.getElementById("decimal1").value;
+    try {
+        var output = parseInt(decimal).toString(8);
+        console.log(output)
+        document.getElementById("output5").innerHTML = `Result is : ${output}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Decimal Number")
+    }    
+}
+
+function decimalHexa(){
+    var decimal = document.getElementById("decimal2").value;
+    try {
+        var output = parseInt(decimal).toString(16);
+        console.log(output)
+        document.getElementById("output6").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Decimal Number")
+    }    
+}
+
+function octalBinary(){
+    var octal = document.getElementById("octal").value;
+    try {
+        var output = parseInt(octal,8).toString(2);
+        document.getElementById("output7").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Octal Number")
+    }    
+}
+
+function octalDecimal(){
+    var octal = document.getElementById("octal1").value;
+    try {
+        var output = parseInt(octal,8).toString(10);
+        document.getElementById("output8").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Octal Number")
+    }    
+}
+
+function octalHexa(){
+    var octal = document.getElementById("octal2").value;
+    try {
+        var output = parseInt(octal,8).toString(16);
+        document.getElementById("output9").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid Octal Number")
+    }    
+}
+
+function hexaBinary(){
+    var hexa = document.getElementById("hexa").value;
+    try {
+        var output = parseInt(hexa,16).toString(2);
+        document.getElementById("output10").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid HexaDecimal Number")
+    }    
+}
+
+function hexaOctal(){
+    var hexa = document.getElementById("hexa1").value;
+    try {
+        var output = parseInt(hexa,16).toString(8);
+        document.getElementById("output11").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid HexaDecimal Number")
+    }    
+}
+
+function hexaDecimal(){
+    var hexa = document.getElementById("hexa2").value;
+    try {
+        var output = parseInt(hexa,16).toString(10);
+        document.getElementById("output12").innerHTML = `Result is : ${output.toUpperCase()}`; 
+    } catch (error) {
+        window.alert("Plz Enter valid HexaDecimal Number")
+    }    
+}
